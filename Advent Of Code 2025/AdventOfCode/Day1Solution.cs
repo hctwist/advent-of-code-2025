7d34177769831f3b734864e2a88be7ca90941edf
Henry Twist <henrytwistprojects@gmail.com>
2025-12-02 22:14:29 +0000

    Initial commit

---
+++ Advent Of Code 2025/AdventOfCode/Day1Solution.cs
@@ -0,0 +1,93 @@
+﻿using AdventOfCode.Framework.Solutions;
+
+namespace AdventOfCode;
+
+[Solution(Day = 1)]
+internal class Day1Solution : ISolution
+{
+    private static readonly int StartingDialValue = 50;
+    private static readonly int MaxDialValue = 100;
+
+    /// <inheritdoc />
+    public string SolveProblem1(ProblemInput input, ISolutionLogger logger)
+    {
+        var rotations = ParseRotations(input.Lines);
+
+        var dialValue = StartingDialValue;
+        var zeroes = 0;
+
+        foreach (var rotation in rotations)
+        {
+            dialValue += rotation;
+
+            if (dialValue % MaxDialValue == 0)
+            {
+                zeroes++;
+            }
+        }
+
+        return zeroes.ToString();
+    }
+
+    /// <inheritdoc />
+    public string SolveProblem2(ProblemInput input, ISolutionLogger logger)
+    {
+        var rotations = ParseRotations(input.Lines);
+
+        var dialValue = StartingDialValue;
+        var zeroes = 0;
+
+        foreach (var rotation in rotations)
+        {
+            var fullRotations = int.Abs(rotation / MaxDialValue);
+            zeroes += fullRotations;
+
+            var previouslyZero = dialValue == 0;
+
+            var remainingRotation = rotation % MaxDialValue;
+            dialValue += remainingRotation;
+
+            var touchedZero = false;
+
+            if (dialValue == 0)
+            {
+                touchedZero = true;
+            }
+            else if (dialValue < 0)
+            {
+                dialValue += MaxDialValue;
+                touchedZero = true;
+            }
+            else if (dialValue >= MaxDialValue)
+            {
+                dialValue -= MaxDialValue;
+                touchedZero = true;
+            }
+
+            if (!previouslyZero && touchedZero)
+            {
+                zeroes++;
+            }
+        }
+
+        return zeroes.ToString();
+    }
+
+    private static IEnumerable<int> ParseRotations(IEnumerable<string> rotations)
+    {
+        foreach (var rotation in rotations)
+        {
+            var directionIndicator = rotation[0];
+            var direction = directionIndicator switch
+            {
+                'L' => -1,
+                'R' => 1,
+                _ => throw new ArgumentException()
+            };
+
+            var amount = int.Parse(rotation[1..]);
+
+            yield return direction * amount;
+        }
+    }
+}
---
+++ Advent Of Code 2025/AdventOfCode/Day2Solution.cs
@@ -0,0 +1,114 @@
+﻿using AdventOfCode.Framework.Solutions;
+
+namespace AdventOfCode;
+
+[Solution(Day = 2)]
+internal class Day2Solution : ISolution
+{
+    /// <inheritdoc />
+    public string SolveProblem1(ProblemInput input, ISolutionLogger logger)
+    {
+        var ranges = ParseRanges(input.Raw);
+
+        var invalidIdSum = 0L;
+
+        foreach (var range in ranges)
+        {
+            for (var id = range.Start; id <= range.End; id++)
+            {
+                var idDigitCount = GetDigitCount(id);
+                var repeatingDigitCount = idDigitCount / 2;
+
+                if (idDigitCount % repeatingDigitCount != 0)
+                {
+                    continue;
+                }
+
+                if (IsRepeating(id, repeatingDigitCount))
+                {
+                    invalidIdSum += id;
+                }
+            }
+        }
+
+        return invalidIdSum.ToString();
+    }
+
+    /// <inheritdoc />
+    public string SolveProblem2(ProblemInput input, ISolutionLogger logger)
+    {
+        var ranges = ParseRanges(input.Raw);
+
+        var invalidIdSum = 0L;
+
+        foreach (var range in ranges)
+        {
+            for (var id = range.Start; id <= range.End; id++)
+            {
+                var digitCount = GetDigitCount(id);
+
+                for (var repeatingDigitCount = 1; repeatingDigitCount < digitCount; repeatingDigitCount++)
+                {
+                    if (digitCount % repeatingDigitCount != 0)
+                    {
+                        continue;
+                    }
+
+                    if (IsRepeating(id, repeatingDigitCount))
+                    {
+                        invalidIdSum += id;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return invalidIdSum.ToString();
+    }
+
+    private static int GetDigitCount(long l)
+    {
+        return (int)double.Floor(double.Log10(l) + 1);
+    }
+
+    private static bool IsRepeating(long id, int digits)
+    {
+        var previousTrimmed = TrimEnd(id, digits, out var remaining);
+
+        while (remaining != 0)
+        {
+            var trimmed = TrimEnd(remaining, digits, out remaining);
+            if (trimmed != previousTrimmed)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static long TrimEnd(long l, int digits, out long remaining)
+    {
+        var factor = checked((long)double.Pow(10, digits));
+
+        remaining = l / factor;
+        return l - remaining * factor;
+    }
+
+    private static IEnumerable<ProductIdRange> ParseRanges(string ranges)
+    {
+        var rangesSplit = ranges.Split(",");
+
+        foreach (var r in rangesSplit)
+        {
+            var rangeSplit = r.Split('-');
+
+            var start = long.Parse(rangeSplit[0]);
+            var end = long.Parse(rangeSplit[1]);
+
+            yield return new ProductIdRange(start, end);
+        }
+    }
+
+    private readonly record struct ProductIdRange(long Start, long End);
+}
