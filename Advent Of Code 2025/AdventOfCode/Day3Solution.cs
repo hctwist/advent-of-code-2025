d636f4d34db7609bb6bc4f718b8b990c6ec8e66c
Henry Twist <henrytwistprojects@gmail.com>
2025-12-03 22:55:02 +0000

    Day 3

---
+++ Advent Of Code 2025/AdventOfCode/Day3Solution.cs
@@ -0,0 +1,60 @@
+﻿using AdventOfCode.Framework.Solutions;
+
+namespace AdventOfCode;
+
+[Solution(Day = 3)]
+internal class Day3Solution : ISolution
+{
+    /// <inheritdoc />
+    public string SolveProblem1(ProblemInput input, ISolutionLogger logger)
+    {
+        var banks = ParseBanks(input.Lines);
+        return CalculateMaximumOutputJoltage(banks, 2).ToString();
+    }
+
+    /// <inheritdoc />
+    public string SolveProblem2(ProblemInput input, ISolutionLogger logger)
+    {
+        var banks = ParseBanks(input.Lines);
+        return CalculateMaximumOutputJoltage(banks, 12).ToString();
+    }
+
+    private static long CalculateMaximumOutputJoltage(IEnumerable<IReadOnlyList<int>> banks, int batteryCount)
+    {
+        var outputJoltage = 0L;
+
+        foreach (var bank in banks)
+        {
+            var joltage = 0L;
+
+            var remainingBatteryCount = batteryCount;
+            var cursor = 0;
+
+            while (remainingBatteryCount > 0)
+            {
+                var maxIndex = cursor;
+
+                for (var i = cursor + 1; i <= bank.Count - remainingBatteryCount; i++)
+                {
+                    if (bank[i] > bank[maxIndex])
+                    {
+                        maxIndex = i;
+                    }
+                }
+
+                joltage = joltage * 10 + bank[maxIndex];
+                remainingBatteryCount--;
+                cursor = maxIndex + 1;
+            }
+
+            outputJoltage += joltage;
+        }
+
+        return outputJoltage;
+    }
+
+    private static IEnumerable<IReadOnlyList<int>> ParseBanks(IEnumerable<string> input)
+    {
+        return input.Select(l => l.Select(c => c - '0').ToList());
+    }
+}
